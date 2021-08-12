163c96a957255473c32cb49fa162e5ca2761993b
BrahianVT <pumasemj@hotmail.com>
2021-08-11 23:37:43 -0600

    Primer post

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,20 +1,20 @@
 const config = {
   // Site info
-  siteTitle: "Gatsby Blog Template", // Site title.
+  siteTitle: "Peligros Nueva Era", // Site title.
   siteTitleShort: "GB Template", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "Gatsby Blog Template", // Alternative site title for SEO.
+  siteTitleAlt: "Nueva Era", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024x1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://gb-template.netlify.com", // Domain of your website without pathPrefix.
   pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "A GatsbyJS Blog Template for blogging purpose.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Informar peligros de la Nueva Era", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteLang: "en",
+  siteLang: "es",
   siteFBAppID: "399626517562189", // FB Application ID for using app insights
   googleAnalyticsID: "UA-96543695-7", // GA tracking ID.
   postDefaultCategoryID: "", // Default category for posts.
   // Common for tag, category pages and widget
-  numberLatestPost: 8,
-  postsPerPage: 6,
+  numberLatestPost: 10,
+  postsPerPage: 10,
   // Use for post
   dateFromFormat: "YYYY-MM-DDTHH:mm:ssZ", // Date format used in the frontmatter.
   dateFormat: "MMMM Do, YYYY", // Date format for display.
@@ -24,10 +24,10 @@ const config = {
   // Use for comment
   lazyLoadComments: true,
   disqusShortname: "gb-template", // Disqus shortname.
-  btnLoadComments: "Load comments",
+  btnLoadComments: "Cargar Comentarios",
   // Use for home page
-  numberLoadmore: 6,
-  btnLoadmore: "Load more",
+  numberLoadmore: 10,
+  btnLoadmore: "Cargar más",
   homeHasLoadmore: true,
   homeHasThumbnail: true,
   homeHeader: "Home",
@@ -36,7 +36,7 @@ const config = {
   pageNotFoundTitle: "Page Not Found", //
   pageNotFoundBtn: "Back to our site",
   pageNotFoundContent:
-    "Looks like you've followed a broken link or entered a URL that doesn't exist on this site.",
+    "Nose encontro la pagina",
   // Use for tag
   pathPrefixTag: "/tag", // Prefix path for tags
   tagHeader: "Posts tagged as", // use in header of tag-template page
@@ -46,40 +46,33 @@ const config = {
   categoryHeader: "Posts in category", // use in header of category-template page
   categoryHasThumbnail: true,
   // Use for widget
-  categoryWidgetTitle: "Categories",
+  categoryWidgetTitle: "Categorias",
   tagWidgetTitle: "Tags",
-  latestPostsWidgetTitle: "Latest posts",
+  latestPostsWidgetTitle: "Ultimos posts",
   linksWidgetTitle: "Links",
   // Use for Google custom search
-  searchWidgetTitle: "Looking for?",
-  searchWidgetPlaceHolder: "Enter keyword",
+  searchWidgetTitle: "Busca algo especifico ?",
+  searchWidgetPlaceHolder: "Busca palabra clave",
   searchEngineID: "008548374781244864787:9ybvtnkbt7o",
   hasSearch: true,
   // Use for links widget
   sidebarSticky: true,
   sidebarLinks: [
     {
-      label: "Complete JavaScript",
-      url: "https://completejavascript.com/",
+      label: "Sitio Cisne Nueva Era",
+      url: "https://www.cisne.org.mx",
     },
-    {
-      label: "Lam Pham Blog",
-      url: "https://phamvanlam.com/",
-    },
-    {
-      label: "Lam Pham Portfolio",
-      url: "http://about.phamvanlam.com/",
-    },
+    ,
   ],
   // Use for user info
-  userName: "Lam Pham", // Username to display in the author segment.
-  userEmail: "completejavascript.super@gmail.com", // Email used for RSS feed"s author segment
-  userTwitter: "CmpltJavaScript", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "Hanoi, Vietnam", // User location to display in the author segment.
+  userName: " Jaime Duarte Martínez", // Username to display in the author segment.
+  userEmail: "https://www.cisne.org.mx/", // Email used for RSS feed"s author segment
+  userTwitter: "CISNE_2012", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "México", // User location to display in the author segment.
   userAvatar:
     "https://www.gravatar.com/avatar/42fd3d526fde1ef76d5002e4ebd303e9.jpg?s=300", // User avatar to display in the author segment.
   userDescription:
-    "Software Engineer, Web Developer, JavaScript Lover & Blogger @completejavascript.", // User description to display in the author segment.
+    "Checar sobre la nueva era", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
@@ -89,23 +82,18 @@ const config = {
     },
     {
       label: "Website",
-      url: "https://completejavascript.com/",
+      url: "https://www.cisne.org.mx/",
       iconClassName: "fas globe",
     },
     {
       label: "Twitter",
-      url: "https://twitter.com/CmpltJavaScript",
+      url: "https://twitter.com/CISNE_2012",
       iconClassName: "fab twitter",
     },
     {
       label: "Facebook",
-      url: "https://www.facebook.com/completejavascript/",
+      url: "https://www.facebook.com/groups/cisneinformativo",
       iconClassName: "fab facebook-f",
-    },
-    {
-      label: "Linkedin",
-      url: "https://www.linkedin.com/in/completejavascript/",
-      iconClassName: "fab linkedin-in",
     },
   ],
   // Use for navigation
@@ -117,33 +105,13 @@ const config = {
   // Use for footer
   socialLinks: [
     {
-      label: "Codepen",
-      url: "https://codepen.io/completejavascript/",
-      iconClassName: "fab codepen",
-    },
-    {
-      label: "FreeCodeCamp",
-      url: "", // Ignore
-      iconClassName: "fab free-code-camp",
-    },
-    {
-      label: "GitHub",
-      url: "https://github.com/completejavascript",
-      iconClassName: "fab github",
-    },
-    {
       label: "Facebook",
-      url: "https://www.facebook.com/completejavascript/",
+      url: "https://www.facebook.com/groups/cisneinformativo",
       iconClassName: "fab facebook-f",
     },
     {
-      label: "Linkedin",
-      url: "https://www.linkedin.com/in/completejavascript/",
-      iconClassName: "fab linkedin-in",
-    },
-    {
       label: "Twitter",
-      url: "https://twitter.com/CmpltJavaScript",
+      url: "https://twitter.com/CISNE_2012",
       iconClassName: "fab twitter",
     },
     {
@@ -161,7 +129,7 @@ const config = {
     { label: "Sitemap", url: "https://gb-template.netlify.com/sitemap.xml" },
   ],
   copyright:
-    "Copyright © 2019-2021 Lam Pham. Unless otherwise noted, all code MIT license.",
+    "Copyright © 2021 ",
   // Use for manifest
   themeColor: "#2196F3", // Used for setting manifest and progress theme colors.
   backgroundColor: "#FFF", // Used for setting manifest background color.
